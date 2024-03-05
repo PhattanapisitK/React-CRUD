66532673f0e3ee97291c0410a669a3de268b74bc
PhattanapisitK <s6506021620091@email.kmutnb.ac.th>
2024-03-05 10:10:13 +0700

    Change Database

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 // JSON data for one book is as follows: 
 // {"id":2,"title":"Fire","author":"Bet"}
 // The id is auto-incremented in the server
-// The server is running at https://node41091-noderest.proen.app.ruk-com.cloud/
+// The server is running at https://node57538-phattanapisitk.proen.app.ruk-com.cloud/books
 
 import BookList from './components/BookList';
 
